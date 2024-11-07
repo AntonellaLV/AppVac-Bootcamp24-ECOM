4b866f2b2484f8a0efba76c86f0eb72956948c02
Antonella Lucero Vetack <118840064+AntonellaLV@users.noreply.github.com>
2024-11-07 13:30:02 -0300

    Delete appvac-pro directory

--- appvac-pro/appvac/appvac/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Proyecto
-
-# Register your models here.
-
-@admin.register(Proyecto)
-class ProyectoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'fecha', 'enlace')  # Campos a mostrar en la lista
-    search_fields = ('titulo',)  # Habilitar búsqueda por título
--- appvac-pro/appvac/appvac/forms.py
+++
@@ -1,8 +0,0 @@
-# portafolio/forms.py
-
-from django import forms
-
-class ContactForm(forms.Form):
-    nombre = forms.CharField(max_length=100, label='Nombre')
-    email = forms.EmailField(label='Correo Electrónico')
-    mensaje = forms.CharField(widget=forms.Textarea, label='Mensaje')
